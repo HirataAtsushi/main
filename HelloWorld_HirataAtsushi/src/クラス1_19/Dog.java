d00dffe920d06b84773009e52703edbb2eed690d
atsus <atsus@ATSUSHI>
2024-01-26 23:43:34 +0900

    課題1.19提出致します。

---
+++ HelloWorld_HirataAtsushi/src/クラス1_19/Dog.java
@@ -0,0 +1,22 @@
+package クラス1_19;
+
+public class Dog {
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	//インスタンス変数定義。
+	String AnimalName;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	//インスタンス変数定義。
+	int AnimalCount;
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	//コンストラクタの定義。
+	public Dog() {
+	//AnimalNameに犬を代入。
+		AnimalName = "犬";
+	}
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	//コンストラクタ（オーバーロード）の定義。
+	public Dog(int i) {
+		//AnimalCounntに引数を代入。
+		AnimalCount = i;
+	}
+}
